c3a800264eeac715e052abec3abaad06990873bb
Maarten de Boer <maarten@djambu.com>
2014-04-13 03:27:55 +0200

    initial repo

---
+++ api/controllers/FooController.js
@@ -0,0 +1,10 @@
+/**
+ * FooController.js 
+ *
+ * @description ::
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+	
+};
---
+++ api/models/Foo.js
@@ -0,0 +1,21 @@
+/**
+ * Foo.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+	attributes: {
+		  name: {
+				type: 'string',
+				required: true
+			},
+			bars:{
+				collection: 'bar',
+				via: 'myfoo'
+			}
+	}
+
+};
